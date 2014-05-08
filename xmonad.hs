ac3ba3c89d388e89d6751de74244c48567b1fe49
Christoph Burschka <christoph@burschka.de>
2014-05-08 22:09:52 +0200

    Fix indentation.

--- xmonad.hs
+++ xmonad.hs
@@ -8,14 +8,14 @@ import Data.Monoid (mappend)
 --alert = dzenConfig return . show
 
 main = do
-     xmproc <- spawnPipe "xmobar"
-     xmonad $ defaultConfig
-     	    {
-		modMask = mod4Mask,
-		keys = keys defaultConfig `mappend`
-		\c -> fromList [
-		   ((0, 0x1008FF11), setMute False >> lowerVolume 4 >> return()),
-		   ((0, 0x1008FF13), setMute False >> raiseVolume 4 >> return()),
-		   ((0, 0x1008FF12), toggleMute >> return())
-		]
-	    }
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        {
+            modMask = mod4Mask,
+            keys = keys defaultConfig `mappend`
+                \c -> fromList [
+                   ((0, 0x1008FF11), setMute False >> lowerVolume 4 >> return()),
+                   ((0, 0x1008FF13), setMute False >> raiseVolume 4 >> return()),
+                   ((0, 0x1008FF12), toggleMute >> return())
+                ]
+        }
